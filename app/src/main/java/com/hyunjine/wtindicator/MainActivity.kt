f07a8fdfeda90e11598eda2098dcb15d14aa6866
hyunjine <hyunjin_98_dev@naver.com>
2022-05-31 00:49:11 +0900

    Init project

---
+++ app/src/main/java/com/hyunjine/wtindicator/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.hyunjine.wtindicator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.ImageView
+import com.hyunjine.wtindicator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var currentSelectedIndicator: ImageView
+
+    private var indicatorSize: Int = 0
+    private var count = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setBinding(layoutInflater)
+        setContentView(binding.root)
+        setViewComponent()
+        setEventListener()
+    }
+
+    private fun setBinding(layoutInflater: LayoutInflater) =
+        ActivityMainBinding.inflate(layoutInflater).also { binding = it }
+
+    private fun setViewComponent() =
+        binding.run {
+            setImageViewDrawable(img1, R.drawable.shape_indicator_selected)
+            currentSelectedIndicator = img1
+        }
+
+    private fun setEventListener() =
+        binding.run {
+            btn.setOnClickListener {
+                setCount()
+                when(count) {
+                    1 -> changeIndicator(img1)
+                    2 -> changeIndicator(img2)
+                    3 -> changeIndicator(img3)
+                }
+            }
+        }
+
+    private fun changeIndicator(currentView: ImageView) {
+        setImageViewDrawable(currentSelectedIndicator, R.drawable.shape_indicator)
+        currentSelectedIndicator = currentView
+        setImageViewDrawable(currentView, R.drawable.shape_indicator_selected)
+    }
+
+    private fun setImageViewDrawable(view: ImageView, drawableId: Int) =
+        view.setImageResource(drawableId)
+
+    private fun setCount() {
+        count++
+        if (count > indicatorSize) count = 1
+    }
+}
---
+++ app/src/main/java/com/hyunjine/wtindicator/WTIndicator.kt
@@ -0,0 +1,22 @@
+package com.hyunjine.wtindicator
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class WTIndicator: ConstraintLayout {
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(
+        context,
+        attrs,
+        defStyle
+    )
+
+    init {
+
+    }
+}
